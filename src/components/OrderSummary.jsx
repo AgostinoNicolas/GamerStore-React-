7858239480711c0144b75400b7be3eaa26328ad9
AgostinoNicolas <nico.agostino@hotmail.com>
2022-05-15 16:18:45 -0300

    se agrego crear orden

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,9 +1,51 @@
 import { Button } from "@material-ui/core";
+import { serverTimestamp, doc, collection, setDoc, increment, updateDoc } from "firebase/firestore";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import db from "./utils/firebaseConfig";
 
 const OrderSummary = () =>{
     const test = useContext(CartContext)
+
+    const createOrder = () => {
+        test.cartList.forEach(async (item) => {
+            const itemRef = doc(db, "products", item.idItem);
+            await updateDoc(itemRef, {
+              stock: increment(-item.qtyItem)
+            });
+          });
+
+    let order = {
+        buyer: {
+            nombre: "Darth Vader",
+            email: "darth@vader.com",
+            telefono: "123456789",
+        },
+        date: serverTimestamp(),
+        items: test.cartList.map(item => ({
+            id: item.idItem,
+            marca: item.nameItem,
+            precio: item.costItem,
+            qty: item.qtyItem,
+        })),
+        total: test.calcTotal(),
+    }
+    console.log(order)
+
+    const createOrderInFirestore = async () => {
+        // Add a new document with a generated id
+        const newOrderRef = doc(collection(db, "orders"));
+        await setDoc(newOrderRef, order);
+        return newOrderRef;
+    }
+        
+    createOrderInFirestore()
+        .then(result => alert('Su pedido ha sido creado. Tome nota del ID de su pedido.\n\n\nID del pedido:' + result.id + '\n\n'))
+        .catch(err => console.log(err));
+        
+    test.removeCart();
+
+    }
 
     return(
         <div className="containerOrderSummary">
@@ -25,7 +67,7 @@ const OrderSummary = () =>{
                     <p>${test.calcTotal()}</p>
                 </div>
                 <div className="containerCheckoutNow">
-                    <Button className="checkoutNow" onClick={checkoutNow} variant="contained" color="secondary">
+                    <Button className="checkoutNow" onClick={createOrder} variant="contained" color="secondary">
                         Finalizar Compra
                     </Button>
                 </div>  
